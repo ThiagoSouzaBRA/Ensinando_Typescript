fdff4cf7cde9d9e1d108c782c476bf5e960ebac1
Thiago Souza <thiagosouza1421@hotmail.com>
2023-07-18 00:23:07 -0300

    First Commit.

---
+++ app.ts
@@ -0,0 +1,32 @@
+// var isBoolean: boolean = false;
+// isBoolean = true;
+// // isBoolean = "Oi" //Erro
+// console.log(isBoolean)
+
+// var isString: String = "Nenhum valor.";
+// // isBoolean = true; //Erro
+// isString = "Acima do limite" 
+// console.log(isString)
+
+
+// isString = `O valor é ${isBoolean}.`;
+// console.log(isString)
+
+// var isNumber: number = 50;
+// isNumber =- 10
+// isNumber =- 1.0
+// isNumber =- .10
+// console.log(isNumber)
+// // isNumber = "Oi" //Erro
+
+
+/*Exercício:
+1) Crie um array com notas de um aluno;
+2) Filtre esse array para retornar somente as notas maiores que 60;
+3) Nesse mesmo array, informe em qual posição aparece a nota 70.
+*/
+let notaAluno:Array<number> = [];
+for(let i = 0; i < 100; i++){
+    notaAluno[i] = (Math.floor(Math.random() * 100) + 50);
+}
+console.log(notaAluno[notaAluno.indexOf(70)])
